9f5e8837d0e73ff5f83759566f02214dd4d9fdd4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-21 10:45:42 -0300

    chore: add nextjs and config

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,8 @@ module.exports = {
     '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1'
   },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   transform: { '.+\\.(ts|tsx)$': 'ts-jest' },
   clearMocks: true
---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Head (): JSX.Element {
+  return (
+    <>
+      <title>Create Next App</title>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </>
+  )
+}
