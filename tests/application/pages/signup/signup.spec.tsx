f9911e437cb20f98e08014d4a1ebf318b3015142
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-24 16:39:03 -0300

    refactor: isolate SUT create

--- tests/application/pages/signup/signup.spec.tsx
+++ tests/application/pages/signup/signup.spec.tsx
@@ -3,9 +3,21 @@ import { SignUp } from '@/application/pages/signup/signup'
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 
+type SutTypes = {
+  container: HTMLElement
+}
+
+const makeSut = (): SutTypes => {
+  const { container } = render(<SignUp/>)
+  return {
+    container
+  }
+}
+
 describe('SignUp', () => {
   it('should load with correct initial state', () => {
-    const { container } = render(<SignUp/>)
+    const { container } = makeSut()
+
     expect(container.getElementsByTagName('label')[0].className).toBe('label bg-danger')
     expect(screen.getByText('ENTRAR')).toBeTruthy()
     expect(screen.getByRole('button')).toBeDisabled()
