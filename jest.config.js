c33605c205836206b60deccbb3593dd89cc3281a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-24 16:33:42 -0300

    chore: add babel

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1'
+    '@/(.+)': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|svg|png|css)$': 'jest-transform-stub',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: [
+    '/node_modules/', '/.next/'
+  ],
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
-  transform: { '.+\\.(ts|tsx)$': 'ts-jest' },
+  testEnvironment: 'jsdom',
   clearMocks: true
 }
