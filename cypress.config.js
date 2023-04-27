d566fef5cc90f26b8f08a771fd6a69d8b1843c53
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-27 15:51:02 -0300

    test: ensure signup enable the button and add the class bg-success pro label

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  fixturesFolder: false,
   video: false,
   e2e: {
     setupNodeEvents (on, config) {
--- tests/e2e/cypress/integration/signup.cy.ts
+++ tests/e2e/cypress/integration/signup.cy.ts
@@ -1,5 +1,9 @@
 import faker from 'faker'
 describe('SignUp', () => {
+  const email = faker.internet.email()
+  const name = faker.name.findName()
+  const password = faker.internet.password()
+  const passwordConfirmation = password
   const invalidEmail = faker.random.word()
   const invalidPasswordConfirmation = faker.random.words(2)
 
@@ -22,5 +26,17 @@ describe('SignUp', () => {
     cy.get("label[for='passwordConfirmation']").should('have.class', 'bg-danger')
     cy.get('button').should('have.attr', 'disabled')
     cy.get("[data-testid='toas']").should('not.exist')
+  })
+
+  it('should enable the button and add the class bg-success pro label', () => {
+    cy.get("input[id='email']").type(email)
+    cy.get("input[id='name']").type(name)
+    cy.get("input[id='password']").type(password)
+    cy.get("input[id='passwordConfirmation']").type(passwordConfirmation)
+    cy.get("label[for='name']").should('have.class', 'bg-success')
+    cy.get("label[for='email']").should('have.class', 'bg-success')
+    cy.get("label[for='password']").should('have.class', 'bg-success')
+    cy.get("label[for='passwordConfirmation']").should('have.class', 'bg-success')
+    cy.get('button').should('not.have.attr', 'disabled')
   })
 })
