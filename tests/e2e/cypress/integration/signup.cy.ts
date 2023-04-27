b99b25e232939ecdc8f12acfc08c15db1ed53b83
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-27 15:33:37 -0300

    test: ensure signup begin with correct initial state

--- tests/e2e/cypress/integration/signup.cy.ts
+++ tests/e2e/cypress/integration/signup.cy.ts
@@ -1,5 +1,13 @@
 describe('SignUp', () => {
-  it('should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('signup')
+  })
+
+  it('should load with correct initial state', () => {
+    cy.get("label[for='name']").should('have.class', 'bg-danger')
+    cy.get("label[for='email']").should('have.class', 'bg-danger')
+    cy.get("label[for='password']").should('have.class', 'bg-danger')
+    cy.get("label[for='passwordConfirmation']").should('have.class', 'bg-danger')
+    cy.get('button').should('have.attr', 'disabled')
   })
 })
