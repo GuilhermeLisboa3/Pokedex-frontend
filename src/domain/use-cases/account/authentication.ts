d77056e7dd4014e08cdf60c6002d2a5c9a41e587
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-28 11:49:44 -0300

    feat: ensure AuthenticationUseCase throw InvalidCredentialsError if HttpClient return 401

--- src/domain/use-cases/account/authentication.ts
+++ src/domain/use-cases/account/authentication.ts
@@ -1,9 +1,14 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { InvalidCredentialsError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient<boolean>) => Authentication
 type Input = { email: string, password: string }
 export type Authentication = (input: Input) => Promise<void>
 
 export const AuthenticationUseCase: Setup = (url, httpClient) => async ({ email, password }) => {
-  await httpClient.request({ url, method: 'post', body: { email, password } })
+  const { statusCode } = await httpClient.request({ url, method: 'post', body: { email, password } })
+  switch (statusCode) {
+    case 401: throw new InvalidCredentialsError()
+    default: return undefined
+  }
 }
