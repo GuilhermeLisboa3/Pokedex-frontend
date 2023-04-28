b9d69de00f7f0a7d19ddd3d66bef9016856c221b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-28 13:19:46 -0300

    test: ensure Login call Validator with correct input

--- tests/application/pages/login/login.spec.tsx
+++ tests/application/pages/login/login.spec.tsx
@@ -1,14 +1,25 @@
 import { Login } from '@/application/pages/login/login'
+import { AccountParams, populateField } from '@/tests/mocks'
+import { type Validator } from '@/application/validation'
 
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import { type MockProxy, mock } from 'jest-mock-extended'
 
 describe('Login', () => {
+  const { email, password } = AccountParams
+  const validator: MockProxy<Validator> = mock()
+
   type SutTypes = { container: HTMLElement }
 
   const makeSut = (): SutTypes => {
-    const { container } = render(<Login/>)
+    const { container } = render(<Login validator={validator}/>)
     return { container }
+  }
+
+  const populateFields = (): void => {
+    populateField('email', email)
+    populateField('password', password)
   }
 
   it('should load with correct initial state', () => {
@@ -18,5 +29,14 @@ describe('Login', () => {
     expect(container.getElementsByTagName('label')[1].className).toBe('label bg-danger')
     expect(screen.getByText('ENTRAR')).toBeTruthy()
     expect(screen.getByRole('button')).toBeDisabled()
+  })
+
+  it('should call Validator with correct input', () => {
+    makeSut()
+
+    populateFields()
+
+    expect(validator.validate).toHaveBeenCalledWith('email', { email })
+    expect(validator.validate).toHaveBeenCalledWith('password', { password })
   })
 })
