f39f06a06a2bf37429b8f06ce0642f841378070e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-30 19:02:52 -0300

    feat: ensure CurrentAccount call setCurrentAccountAdapter with correct values

--- src/application/contexts/account.ts
+++ src/application/contexts/account.ts
@@ -3,7 +3,6 @@ import { createContext } from 'react'
 
 type Props = {
   setCurrentAccount: (account: Account) => void
-  getCurrentAccount: () => Account
 }
 
 export const AccountContext = createContext<Props>(null as any)
