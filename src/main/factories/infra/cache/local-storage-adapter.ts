d7773ea28928a4614d8bb2ed5e1ca6b13d75a08b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-30 19:01:25 -0300

    feat: add factory local storage

---
+++ src/main/factories/infra/cache/local-storage-adapter.ts
@@ -0,0 +1,4 @@
+import { type SetStorage } from '@/domain/contracts/cache'
+import { LocalStorageAdapter } from '@/infra/cache'
+
+export const makeLocalStorageAdapter = (): SetStorage => new LocalStorageAdapter()
