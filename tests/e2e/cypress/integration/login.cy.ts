ba835cfa58c8d62366c95d38e185c7a620d25b66
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-01 10:24:08 -0300

    test: ensure Login keep the button disabled if form is invalid

--- tests/e2e/cypress/integration/login.cy.ts
+++ tests/e2e/cypress/integration/login.cy.ts
@@ -1,4 +1,13 @@
+import faker from 'faker'
+
 describe('Login', () => {
+  const invalidEmail = faker.random.word()
+
+  const populateFields = (email = faker.internet.email(), password = faker.internet.password()): void => {
+    cy.getInputById('email').focus().type(email)
+    cy.getInputById('password').focus().type(password)
+  }
+
   beforeEach(() => {
     cy.visit('login')
   })
@@ -12,5 +21,12 @@ describe('Login', () => {
   it('should show the toas if the user has registered a new account', () => {
     cy.visit('login?registred=true')
     cy.get("[data-testid='toas']").should('exist').should('have.text', 'Registrado com sucesso!')
+  })
+
+  it('should keep the button disabled if form is invalid', () => {
+    populateFields(invalidEmail)
+    cy.getLabelByFor('email').should('have.class', 'bg-danger')
+    cy.get('button').should('have.attr', 'disabled')
+    cy.get("[data-testid='toas']").should('not.exist')
   })
 })
