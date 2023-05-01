bf74e7b6483e61e7644ce94ec5df0e8122d466a8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-01 14:59:17 -0300

    refactor: change url ListPokemonsUseCase

--- src/domain/use-cases/pokemon/list-pokemons.ts
+++ src/domain/use-cases/pokemon/list-pokemons.ts
@@ -2,13 +2,14 @@ import { type HttpClient } from '@/domain/contracts/http'
 import { UnexpectedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient<{ results: Array<{ name: string }> }>) => ListPokemons
+type Input = { page: number, perPage: number }
 type Output = Array<{ name: string }>
-export type ListPokemons = () => Promise<Output>
+export type ListPokemons = (input: Input) => Promise<Output>
 
-export const ListPokemonsUseCase: Setup = (url, httpClient) => async () => {
-  const { statusCode, data } = await httpClient.request({ url, method: 'get' })
-  switch (statusCode) {
-    case 200: return data!.results
+export const ListPokemonsUseCase: Setup = (url, httpClient) => async ({ page, perPage = 25 }) => {
+  const listNamePokemons = await httpClient.request({ url: `${url}/pokemon?limit=${perPage}&offset=${page}`, method: 'get' })
+  switch (listNamePokemons.statusCode) {
+    case 200: return listNamePokemons.data!.results
     default: throw new UnexpectedError()
   }
 }
--- tests/domain/use-cases/pokemon/list-pokemons.spec.ts
+++ tests/domain/use-cases/pokemon/list-pokemons.spec.ts
@@ -6,6 +6,8 @@ import { mock } from 'jest-mock-extended'
 import { UnexpectedError } from '@/domain/errors'
 
 describe('ListPokemonsUseCase', () => {
+  const page: number = 25
+  const perPage: number = 25
   let sut: ListPokemons
   const { name } = PokemonParams
   const { url } = httpClientParams
@@ -20,22 +22,22 @@ describe('ListPokemonsUseCase', () => {
   })
 
   it('should call HttpClient with correct values', async () => {
-    await sut()
+    await sut({ page, perPage })
 
-    expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'get' })
+    expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/pokemon?limit=${perPage}&offset=${page}`, method: 'get' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
   })
 
   it('should throw UnexpectedError if HttpClient return error', async () => {
     httpClient.request.mockResolvedValueOnce({ statusCode: 500 })
 
-    const promise = sut()
+    const promise = sut({ page, perPage })
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
   it('should return an pokemon if HttpClient return 200', async () => {
-    const result = await sut()
+    const result = await sut({ page, perPage })
 
     expect(result).toEqual([name])
   })
