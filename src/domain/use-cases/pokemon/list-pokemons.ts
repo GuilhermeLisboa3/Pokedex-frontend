52d9c3714e862121546627c3aab07bca10177470
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-01 12:02:33 -0300

    feat: ensure ListPokemonsUseCase throw UnexpectedError if HttpClient return error

--- src/domain/use-cases/pokemon/list-pokemons.ts
+++ src/domain/use-cases/pokemon/list-pokemons.ts
@@ -1,8 +1,13 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { UnexpectedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient<boolean>) => ListPokemons
 export type ListPokemons = () => Promise<void>
 
 export const ListPokemonsUseCase: Setup = (url, httpClient) => async () => {
-  await httpClient.request({ url, method: 'get' })
+  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  switch (statusCode) {
+    case 200: return undefined
+    default: throw new UnexpectedError()
+  }
 }
