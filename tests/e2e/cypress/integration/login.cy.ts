527b8cfe95fca1d2eb00f1aff5a2d68f81a30e42
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-01 10:21:56 -0300

    test: ensure Login show the toas if the user has registered a new account

--- tests/e2e/cypress/integration/login.cy.ts
+++ tests/e2e/cypress/integration/login.cy.ts
@@ -8,4 +8,9 @@ describe('Login', () => {
     cy.getLabelByFor('password').should('have.class', 'bg-danger')
     cy.get('button').should('have.attr', 'disabled')
   })
+
+  it('should show the toas if the user has registered a new account', () => {
+    cy.visit('login?registred=true')
+    cy.get("[data-testid='toas']").should('exist').should('have.text', 'Registrado com sucesso!')
+  })
 })
