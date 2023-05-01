91f2a9c30ab2d9e475313f2e787f55894495b262
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-01 12:27:46 -0300

    feat: ensure ListPokemonsUseCase return an pokemon if HttpClient return 200

---
+++ src/domain/models/pokemon.ts
@@ -0,0 +1,13 @@
+
+export interface Pokemon {
+  id: string
+  name: string
+  sprites: { front_default: string }
+  types: Array<{ type: { name: string } }>
+  height: number
+  weight: number
+  base_experience: number
+  abilities: Array<{ ability: { name: string } }>
+  species: { url: string }
+  stats: Array<{ base_stat: number }>
+}
--- src/domain/use-cases/pokemon/list-pokemons.ts
+++ src/domain/use-cases/pokemon/list-pokemons.ts
@@ -1,13 +1,15 @@
 import { type HttpClient } from '@/domain/contracts/http'
 import { UnexpectedError } from '@/domain/errors'
+import { type Pokemon } from '@/domain/models'
 
-type Setup = (url: string, httpClient: HttpClient<boolean>) => ListPokemons
-export type ListPokemons = () => Promise<void>
+type Setup = (url: string, httpClient: HttpClient<Pokemon>) => ListPokemons
+type Output = Pokemon
+export type ListPokemons = () => Promise<Output>
 
 export const ListPokemonsUseCase: Setup = (url, httpClient) => async () => {
-  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  const { statusCode, data } = await httpClient.request({ url, method: 'get' })
   switch (statusCode) {
-    case 200: return undefined
+    case 200: return data!
     default: throw new UnexpectedError()
   }
 }
