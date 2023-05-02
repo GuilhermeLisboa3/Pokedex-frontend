a641f2141fa8fab32f591b684e1047fda8f915c4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-02 11:24:23 -0300

    test: ensure ListPokemonUseCase call HttpClient if HttpClient returns 200

--- tests/domain/use-cases/pokemon/list-pokemons.spec.ts
+++ tests/domain/use-cases/pokemon/list-pokemons.spec.ts
@@ -14,7 +14,7 @@ describe('ListPokemonsUseCase', () => {
   const httpClient = mock<HttpClient>()
 
   beforeAll(() => {
-    httpClient.request.mockResolvedValue({ statusCode: 200, data: { results: [name] } })
+    httpClient.request.mockResolvedValue({ statusCode: 200, data: { results: [{ name }] } })
   })
 
   beforeEach(() => {
@@ -25,7 +25,6 @@ describe('ListPokemonsUseCase', () => {
     await sut({ page, perPage })
 
     expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/pokemon?limit=${perPage}&offset=${page}`, method: 'get' })
-    expect(httpClient.request).toHaveBeenCalledTimes(1)
   })
 
   it('should throw UnexpectedError if HttpClient return error', async () => {
@@ -36,9 +35,9 @@ describe('ListPokemonsUseCase', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
-  it('should return an pokemon if HttpClient return 200', async () => {
-    const result = await sut({ page, perPage })
+  it('should call HttpClient if HttpClient returns 200', async () => {
+    await sut({ page, perPage })
 
-    expect(result).toEqual([name])
+    expect(httpClient.request).toHaveBeenNthCalledWith(2, { url: `${url}/pokemon/${name}`, method: 'get' })
   })
 })
