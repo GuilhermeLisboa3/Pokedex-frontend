a5a38c306ab37522ad09d508686cfd2656be3646
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-02 19:50:37 -0300

    feat: ensure Home call ListPokemons

--- src/application/pages/home/home.tsx
+++ src/application/pages/home/home.tsx
@@ -1,10 +1,15 @@
 import './styles.scss'
 import { EmptyCardPokemon, Footer, Header } from '@/application/components'
+import { type ListPokemons } from '@/domain/use-cases/pokemon'
 
 import { Container } from 'reactstrap'
 import { Pagination } from './components'
+import { useEffect } from 'react'
 
-export const Home: React.FC = () => {
+type Props = { listPokemons: ListPokemons }
+
+export const Home: React.FC<Props> = ({ listPokemons }: Props) => {
+  useEffect(() => { listPokemons({ page: 0, perPage: 25 }) })
   return (
     <>
       <Container className='homeContainer'>
