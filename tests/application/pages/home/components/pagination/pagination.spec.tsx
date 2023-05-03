c8b57008451441d1aa23bc3038d126c38d915f7b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-03 13:48:27 -0300

    test: ensure Pagination call setPage with correct value

---
+++ tests/application/pages/home/components/pagination/pagination.spec.tsx
@@ -0,0 +1,19 @@
+import { Pagination } from '@/application/pages/home/components'
+
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+
+describe('Pagination', () => {
+  const setPageSpy = jest.fn()
+  it('should call setPage with correct value', async () => {
+    render(<Pagination count={50} perPage={25} page={2} setPage={setPageSpy}/>)
+    fireEvent.click(screen.getAllByRole('button')[0])
+    expect(setPageSpy).toHaveBeenCalledWith(1)
+  })
+
+  it('should call setPage with correct value', async () => {
+    render(<Pagination count={50} perPage={25} page={0} setPage={setPageSpy}/>)
+    fireEvent.click(screen.getAllByRole('button')[1])
+    expect(setPageSpy).toHaveBeenCalledWith(1)
+  })
+})
