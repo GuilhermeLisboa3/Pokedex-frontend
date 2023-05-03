22fdfcca1633d1b847568610e3a5c12d2c2419f4
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-03 12:47:33 -0300

    feat: ensure Home render CardPokemon on success

--- src/application/pages/home/home.tsx
+++ src/application/pages/home/home.tsx
@@ -1,15 +1,20 @@
 import './styles.scss'
-import { EmptyCardPokemon, Footer, Header } from '@/application/components'
+import { EmptyCardPokemon, Footer, Header, CardPokemon } from '@/application/components'
+import { type Pokemon } from '@/domain/models'
 import { type ListPokemons } from '@/domain/use-cases/pokemon'
 
 import { Container } from 'reactstrap'
 import { Pagination } from './components'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 type Props = { listPokemons: ListPokemons }
 
 export const Home: React.FC<Props> = ({ listPokemons }: Props) => {
-  useEffect(() => { listPokemons({ page: 0, perPage: 25 }) })
+  const [listPokemon, setListPokemon] = useState<Pokemon[]>([])
+
+  useEffect(() => {
+    listPokemons({ page: 0, perPage: 25 }).then(result => { setListPokemon(result.pokemons) })
+  })
   return (
     <>
       <Container className='homeContainer'>
@@ -17,7 +22,10 @@ export const Home: React.FC<Props> = ({ listPokemons }: Props) => {
         <main>
           <Pagination/>
           <div className='listPokemons'>
-            <EmptyCardPokemon/>
+            { listPokemon.length > 0
+              ? listPokemon.map(pokemon => (<CardPokemon key={pokemon.id} pokemon={pokemon}/>))
+              : <EmptyCardPokemon/>
+            }
           </div>
         </main>
         <Footer/>
