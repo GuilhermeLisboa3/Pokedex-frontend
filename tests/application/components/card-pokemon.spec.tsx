3ba635284de88f145324457f22bff44c1f60ce42
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-03 11:09:13 -0300

    test: ensure CardPokemon render with correct values and two typesPokemon

--- tests/application/components/card-pokemon.spec.tsx
+++ tests/application/components/card-pokemon.spec.tsx
@@ -3,6 +3,7 @@ import { CardPokemon } from '@/application/components'
 
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import faker from 'faker'
 
 describe('CardPokemon', () => {
   it('should render with correct values', () => {
@@ -14,5 +15,18 @@ describe('CardPokemon', () => {
     expect(container.querySelector('.typesPokemon')?.children).toHaveLength(1)
     expect(container.querySelector('.typesPokemon')?.children[0]).toHaveTextContent(PokemonParams.types[0].type.name)
     expect(container.querySelector('.typesPokemon')?.children[1]).toBeUndefined()
+  })
+
+  it('should render with correct values and two typesPokemon', () => {
+    const type1 = faker.name.findName()
+    const type2 = faker.name.findName()
+    const { container } = render(<CardPokemon pokemon={{ ...PokemonParams, types: [{ type: { name: type1 } }, { type: { name: type2 } }] }}/>)
+    expect(screen.getByRole('img')).toHaveAttribute('src', PokemonParams.sprites.front_default)
+    expect(screen.getByRole('img')).toHaveAttribute('alt', PokemonParams.name)
+    expect(container.querySelector('.namePokemon')).toHaveTextContent(PokemonParams.name)
+    expect(container.querySelector('.idPokemon')).toHaveTextContent(PokemonParams.id)
+    expect(container.querySelector('.typesPokemon')?.children).toHaveLength(2)
+    expect(container.querySelector('.typesPokemon')?.children[0]).toHaveTextContent(type1)
+    expect(container.querySelector('.typesPokemon')?.children[1]).toHaveTextContent(type2)
   })
 })
