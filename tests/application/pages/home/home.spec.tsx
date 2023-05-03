bf31b249285ec9d374ade16ef0f3294b60543110
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-03 12:48:11 -0300

    test: ensure Home render CardPokemon on success

--- tests/application/pages/home/home.spec.tsx
+++ tests/application/pages/home/home.spec.tsx
@@ -1,7 +1,8 @@
 import { Home } from '@/application/pages/home/home'
+import { PokemonParams } from '@/tests/mocks'
 
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 
 describe('Home', () => {
   const listPokemons: jest.Mock = jest.fn()
@@ -12,16 +13,30 @@ describe('Home', () => {
     return { container }
   }
 
-  it('should load with correct initial state', () => {
+  beforeAll(() => {
+    listPokemons.mockResolvedValue({ count: 1, pokemons: [PokemonParams, { ...PokemonParams, id: '1' }, { ...PokemonParams, id: '2' }] })
+  })
+
+  it('should load with correct initial state', async () => {
     const { container } = makeSut()
 
     expect(container.querySelectorAll('.emptyCardPokemon')).toHaveLength(8)
+    await waitFor(() => container.querySelector('.listPokemons'))
   })
 
-  it('should call ListPokemons', () => {
-    makeSut()
+  it('should call ListPokemons', async () => {
+    const { container } = makeSut()
+    await waitFor(() => container.querySelector('.listPokemons'))
 
     expect(listPokemons).toHaveBeenCalledWith({ page: 0, perPage: 25 })
     expect(listPokemons).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should render CardPokemon on success', async () => {
+    const { container } = makeSut()
+    await waitFor(() => container.querySelector('.listPokemons'))
+
+    expect(container.querySelectorAll('.cardPokemon')).toHaveLength(3)
+    expect(container.querySelectorAll('.emptyCardPokemon')).toHaveLength(0)
   })
 })
