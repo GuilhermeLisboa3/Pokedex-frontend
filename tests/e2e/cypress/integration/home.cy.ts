5bb3839ae313e7b19e6547733d651d67b5087ce0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-04 12:38:04 -0300

    test: ensure Home present pokemon list

--- tests/e2e/cypress/integration/home.cy.ts
+++ tests/e2e/cypress/integration/home.cy.ts
@@ -1,7 +1,9 @@
-import { mockUnexpectedError } from '../mocks/http-mocks'
+import { mockOk, mockUnexpectedError } from '../mocks/http-mocks'
 
 describe('Home', () => {
-  const mockError = (method: any): void => { method('GET', /pokemon/) }
+  const mockError = (method: any): void => { method('GET', /pokemon?/) }
+  const mockListPokemonSuccess = (): void => { mockOk('GET', /pokemon?/, 'list-pokemon') }
+  const mockSuccess = (): void => { mockOk('GET', /pokemon[/]/, 'pokemon') }
 
   beforeEach(() => {
   })
@@ -12,5 +14,17 @@ describe('Home', () => {
     cy.visit('')
 
     cy.get('.error span').should('have.text', 'Algo deu errado. Tente novamente!')
+  })
+
+  it('should present pokemon list', () => {
+    mockListPokemonSuccess()
+    mockSuccess()
+
+    cy.visit('')
+
+    cy.get('.imgPokemon').should('have.attr', 'src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png')
+    cy.get('.idPokemon').should('have.text', 'Nº9')
+    cy.get('.namePokemon').should('have.text', 'blastoise')
+    cy.get('.type').should('have.text', 'water')
   })
 })
