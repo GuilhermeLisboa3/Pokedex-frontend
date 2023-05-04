3dad5e3da1793591eea81a4d681a09752fd2db4f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-04 16:00:14 -0300

    refactor: move AccountContext to layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
+'use client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/application/styles/global.scss'
 import '@/application/styles/typesPokemons.scss'
+import { AccountContext } from '@/application/contexts'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters'
 
 const RootLayout = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <AccountContext.Provider value={{ setCurrentAccount: setCurrentAccountAdapter, getCurrentAccount: getCurrentAccountAdapter }}>
+        {children}
+      </AccountContext.Provider>
+      </body>
     </html>
   )
 }
