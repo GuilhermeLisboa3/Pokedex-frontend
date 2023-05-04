0dda862dbebe64277fbb73b02f34b4d30adddefe
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-04 15:15:08 -0300

    test: ensure currentAccount call getCurrentAccountAdapter with correct values

--- tests/main/adapters/current-account.spec.ts
+++ tests/main/adapters/current-account.spec.ts
@@ -1,17 +1,31 @@
 import { AccountParams } from '@/tests/mocks'
 import { LocalStorageAdapter } from '@/infra/cache'
-import { setCurrentAccountAdapter } from '@/main/adapters'
+import { setCurrentAccountAdapter, getCurrentAccountAdapter } from '@/main/adapters'
 
 jest.mock('@/infra/cache/local-storage-adapter')
 describe('currentAccount', () => {
   const { name, email, token } = AccountParams
   const setSpy: jest.Mock = jest.fn()
+  const getSpy: jest.Mock = jest.fn()
 
-  jest.mocked(LocalStorageAdapter).mockImplementation(jest.fn().mockImplementation(() => ({ set: setSpy })))
+  jest.mocked(LocalStorageAdapter).mockImplementation(jest.fn().mockImplementation(() => ({ set: setSpy, get: getSpy })))
 
-  it('should call setCurrentAccountAdapter with correct values', () => {
-    setCurrentAccountAdapter({ name, email, token })
+  describe('set()', () => {
+    it('should call setCurrentAccountAdapter with correct values', () => {
+      setCurrentAccountAdapter({ name, email, token })
 
-    expect(setSpy).toHaveBeenCalledWith({ key: 'pokemon-token', value: { name, email, token } })
+      expect(setSpy).toHaveBeenCalledWith({ key: 'pokemon-token', value: { name, email, token } })
+    })
+  })
+
+  describe('set()', () => {
+    it('should call getCurrentAccountAdapter with correct values', () => {
+      getSpy.mockReturnValueOnce({ name, email, token })
+
+      const account = getCurrentAccountAdapter()
+
+      expect(getSpy).toHaveBeenCalledWith({ key: 'pokemon-token' })
+      expect(account).toEqual({ name, email, token })
+    })
   })
 })
