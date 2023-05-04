2335dbf9c8d56f6d73ecca204a45cc452ef721bd
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-04 14:25:51 -0300

    feat: add contracts getStorage

--- src/domain/contracts/cache/storage.ts
+++ src/domain/contracts/cache/storage.ts
@@ -5,3 +5,11 @@ export interface SetStorage {
 export namespace SetStorage {
   export type Input = { key: string, value: object }
 }
+
+export interface GetStorage {
+  get: (input: GetStorage.Input) => any
+}
+
+export namespace GetStorage {
+  export type Input = { key: string }
+}
