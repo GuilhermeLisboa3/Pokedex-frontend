fe4f17252cacdb86d8097cc5f50a57ae035d7c4c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-05 16:27:58 -0300

    feat: ensure GetDataPokemonUseCase return data pokemon on success

--- src/domain/use-cases/api-pokemon/get-data-pokemon.ts
+++ src/domain/use-cases/api-pokemon/get-data-pokemon.ts
@@ -4,12 +4,13 @@ import { type Pokemon } from '@/domain/models'
 
 type Setup = (url: string, httpClient: HttpClient<Pokemon>) => GetDataPokemon
 type Input = { name: string }
-export type GetDataPokemon = (input: Input) => Promise<void>
+type Output = Pokemon
+export type GetDataPokemon = (input: Input) => Promise<Output>
 
 export const GetDataPokemonUseCase: Setup = (url, httpClient) => async ({ name }) => {
-  const { statusCode } = await httpClient.request({ url: `${url}/pokemon/${name}`, method: 'get' })
+  const { statusCode, data } = await httpClient.request({ url: `${url}/pokemon/${name}`, method: 'get' })
   switch (statusCode) {
-    case 200: return undefined
+    case 200: return data!
     default: throw new UnexpectedError()
   }
 }
