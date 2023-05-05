9b40834120bd652fa0389367c01151f70ea15c12
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-05 16:20:51 -0300

    test: ensure GetDataPokemonUseCase call HttpClient with correct values

---
+++ tests/domain/use-cases/api-pokemon/get-data-pokemon.spec.ts
@@ -0,0 +1,22 @@
+import { type GetDataPokemon, GetDataPokemonUseCase } from '@/domain/use-cases/api-pokemon'
+import { type HttpClient } from '@/domain/contracts/http'
+import { httpClientParams, PokemonParams } from '@/tests/mocks'
+
+import { mock } from 'jest-mock-extended'
+
+describe('GetDataPokemonUseCase', () => {
+  let sut: GetDataPokemon
+  const { url } = httpClientParams
+  const { name } = PokemonParams
+  const httpClient = mock<HttpClient>()
+
+  beforeEach(() => {
+    sut = GetDataPokemonUseCase(url, httpClient)
+  })
+
+  it('should call HttpClient with correct values', async () => {
+    await sut({ name })
+
+    expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/pokemon/${name}`, method: 'get' })
+  })
+})
