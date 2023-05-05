bde3a73ea6b67f6b1f4c9f698777138d732afa0d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-05 12:48:44 -0300

    test: ensure Auth show modal if click button

--- tests/application/components/auth.spec.tsx
+++ tests/application/components/auth.spec.tsx
@@ -1,20 +1,22 @@
 import { Auth } from '@/application/components/header/components'
 
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 describe('Auth', () => {
-  type SutTypes = { container: HTMLElement }
-
-  const makeSut = (): SutTypes => {
-    const { container } = render(<Auth/>)
-    return { container }
-  }
+  const makeSut = (): void => { render(<Auth/>) }
 
   it('should load with correct initial state', () => {
     makeSut()
     expect(screen.getAllByRole('button')[0]).toHaveClass('auth-icon-navigate')
     expect(screen.getAllByRole('button')[1]).toHaveClass('auth-icon-navigate')
     expect(screen.queryByTestId('modal')).not.toBeInTheDocument()
+  })
+
+  it('should show modal if click button', async () => {
+    makeSut()
+    fireEvent.click(screen.getAllByRole('button')[0])
+    expect(screen.getByText('Deletar conta')).toBeInTheDocument()
+    expect(screen.getByText('Sair')).toBeInTheDocument()
   })
 })
--- tests/application/components/card-pokemon.spec.tsx
+++ tests/application/components/card-pokemon.spec.tsx
@@ -7,10 +7,10 @@ import faker from 'faker'
 
 describe('CardPokemon', () => {
   it('should render with correct values', () => {
-    const { container } = render(<CardPokemon pokemon={PokemonParams}/>)
+    const { container } = render(<CardPokemon pokemon={{ ...PokemonParams, name: 'any_name' }}/>)
     expect(screen.getByRole('img')).toHaveAttribute('src', PokemonParams.sprites.front_default)
-    expect(screen.getByRole('img')).toHaveAttribute('alt', PokemonParams.name)
-    expect(container.querySelector('.card-pokemon-name-pokemon')).toHaveTextContent(PokemonParams.name)
+    expect(screen.getByRole('img')).toHaveAttribute('alt', 'any_name')
+    expect(container.querySelector('.card-pokemon-name-pokemon')).toHaveTextContent('any_name')
     expect(container.querySelector('.card-pokemon-id-pokemon')).toHaveTextContent(PokemonParams.id)
     expect(container.querySelector('.card-pokemon-types-pokemon')?.children).toHaveLength(1)
     expect(container.querySelector('.card-pokemon-types-pokemon')?.children[0]).toHaveTextContent(PokemonParams.types[0].type.name)
@@ -20,10 +20,10 @@ describe('CardPokemon', () => {
   it('should render with correct values and two typesPokemon', () => {
     const type1 = faker.name.findName()
     const type2 = faker.name.findName()
-    const { container } = render(<CardPokemon pokemon={{ ...PokemonParams, types: [{ type: { name: type1 } }, { type: { name: type2 } }] }}/>)
+    const { container } = render(<CardPokemon pokemon={{ ...PokemonParams, types: [{ type: { name: type1 } }, { type: { name: type2 } }], name: 'any_name' }}/>)
     expect(screen.getByRole('img')).toHaveAttribute('src', PokemonParams.sprites.front_default)
-    expect(screen.getByRole('img')).toHaveAttribute('alt', PokemonParams.name)
-    expect(container.querySelector('.card-pokemon-name-pokemon')).toHaveTextContent(PokemonParams.name)
+    expect(screen.getByRole('img')).toHaveAttribute('alt', 'any_name')
+    expect(container.querySelector('.card-pokemon-name-pokemon')).toHaveTextContent('any_name')
     expect(container.querySelector('.card-pokemon-id-pokemon')).toHaveTextContent(PokemonParams.id)
     expect(container.querySelector('.card-pokemon-types-pokemon')?.children).toHaveLength(2)
     expect(container.querySelector('.card-pokemon-types-pokemon')?.children[0]).toHaveTextContent(type1)
