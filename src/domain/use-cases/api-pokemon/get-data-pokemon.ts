7d3ce85c007aafdf6f6fe9191a9302e7ca70a8db
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-07 14:27:07 -0300

    feat: ensure GetDataPokemonUseCase return pokemons on success

--- src/domain/use-cases/api-pokemon/get-data-pokemon.ts
+++ src/domain/use-cases/api-pokemon/get-data-pokemon.ts
@@ -1,13 +1,17 @@
 import { type HttpResponse, type HttpClient } from '@/domain/contracts/http'
 import { UnexpectedError } from '@/domain/errors'
-import { type Pokemon } from '@/domain/models'
+import { type Pokemon, type SpeciesPokemon } from '@/domain/models'
 
 type Setup = (url: string, httpClient: HttpClient) => GetDataPokemon
 type Input = { name: string }
-export type GetDataPokemon = (input: Input) => Promise<void>
+type Output = { pokemon: Pokemon, description: string }
+export type GetDataPokemon = (input: Input) => Promise<Output>
 
 export const GetDataPokemonUseCase: Setup = (url, httpClient) => async ({ name }) => {
   const pokemon: HttpResponse<Pokemon> = await httpClient.request({ url: `${url}/pokemon/${name}`, method: 'get' })
   if (pokemon.statusCode !== 200) throw new UnexpectedError()
-  await httpClient.request({ url: `${pokemon.data!.species.url}`, method: 'get' })
+  const pokemonDescription: HttpResponse<SpeciesPokemon> = await httpClient.request({ url: `${pokemon.data!.species.url}`, method: 'get' })
+  const description = pokemonDescription.data?.flavor_text_entries.filter(description => description.language.name === 'en')[0].flavor_text
+  const removesSymbols = description!.replace('', ' ')
+  return { pokemon: pokemon.data!, description: removesSymbols }
 }
