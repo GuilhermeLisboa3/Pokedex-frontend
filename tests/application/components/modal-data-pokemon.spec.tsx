866716c0a9b408a2b3fb86711516ef62fde1f17c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-08 14:10:43 -0300

    test: ensure ModalDataPokemon render with correct values

---
+++ tests/application/components/modal-data-pokemon.spec.tsx
@@ -0,0 +1,27 @@
+import { PokemonParams } from '@/tests/mocks'
+import { ModalDataPokemon } from '@/application/components/modal-data-pokemon/modal-data-pokemon'
+
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import faker from 'faker'
+
+describe('ModalDataPokemon', () => {
+  const { name, id, types, abilities, height, weight, stats } = PokemonParams
+  it('should render with correct values', () => {
+    const type2 = faker.name.findName()
+    render(<ModalDataPokemon pokemon={PokemonParams} pokemonDescription='any_description'/>)
+    expect(screen.getByRole('img')).toHaveAttribute('src', PokemonParams.sprites.front_default)
+    expect(screen.getByRole('img')).toHaveAttribute('alt', name)
+    expect(screen.getByText(name)).toBeInTheDocument()
+    expect(screen.getByText(`#${id}`)).toBeInTheDocument()
+    expect(screen.getByText(types[0].type.name)).toBeInTheDocument()
+    expect(screen.getByText(types[0].type.name)).toHaveClass(`type ${types[0].type.name}`)
+    expect(screen.queryByText(type2)).not.toBeInTheDocument()
+    expect(screen.getByText('any_description')).toBeInTheDocument()
+    expect(screen.getByText(abilities[0].ability.name)).toBeInTheDocument()
+    expect(screen.getByText(abilities[0].ability.name)).toHaveClass(`br-${types[0].type.name}`)
+    expect(screen.getByText(height)).toBeInTheDocument()
+    expect(screen.getByText(`${weight}kg`)).toBeInTheDocument()
+    expect(screen.getByText(stats[0].base_stat)).toBeInTheDocument()
+  })
+})
