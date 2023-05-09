34f8651c3f889e3336278a6d3328cd609021f88c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-09 11:25:32 -0300

    feat: add factory GetDataPokemon

--- src/main/factories/application/page/home.tsx
+++ src/main/factories/application/page/home.tsx
@@ -1,8 +1,8 @@
-import { makeListPokemon } from '@/main/factories/domain/use-cases'
+import { makeListPokemon, makeGetDataPokemon } from '@/main/factories/domain/use-cases'
 import { Home } from '@/application/pages/home/home'
 
 import React from 'react'
 
 export const MakeHome: React.FC = () => (
-  <Home listPokemons={makeListPokemon()} />
+  <Home listPokemons={makeListPokemon()} getDataPokemon={makeGetDataPokemon()} />
 )
