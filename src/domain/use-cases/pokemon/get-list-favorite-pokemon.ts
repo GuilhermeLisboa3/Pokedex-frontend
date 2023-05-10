4553b93ce7b8ffb0d116a69217dc3daf40f510a9
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-10 14:00:55 -0300

    feat: ensure GetListFavoritePokemonUseCase throw AccessDeniedError HttpClient return 403

--- src/domain/use-cases/pokemon/get-list-favorite-pokemon.ts
+++ src/domain/use-cases/pokemon/get-list-favorite-pokemon.ts
@@ -1,8 +1,13 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { AccessDeniedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => GetListFavoritePokemon
 export type GetListFavoritePokemon = () => Promise<void>
 
 export const GetListFavoritePokemonUseCase: Setup = (url, httpClient) => async () => {
-  await httpClient.request({ url, method: 'get' })
+  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  switch (statusCode) {
+    case 403: throw new AccessDeniedError()
+    default: return undefined
+  }
 }
