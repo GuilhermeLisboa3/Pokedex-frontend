3cc84f40cfc32d233586d4bceb83bfe49a0db342
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-11 15:21:14 -0300

    test: ensure AddPokemonUseCase call HttpClient with correct values

---
+++ tests/domain/use-cases/pokemon/add-pokemon.spec.ts
@@ -0,0 +1,22 @@
+import { type AddPokemon, AddPokemonUseCase } from '@/domain/use-cases/pokemon'
+import { type HttpClient } from '@/domain/contracts/http'
+import { httpClientParams, PokemonParams } from '@/tests/mocks'
+
+import { mock } from 'jest-mock-extended'
+
+describe('AddPokemonUseCase', () => {
+  let sut: AddPokemon
+  const { url } = httpClientParams
+  const httpClient = mock<HttpClient>()
+
+  beforeEach(() => {
+    sut = AddPokemonUseCase(url, httpClient)
+  })
+
+  it('should call HttpClient with correct values', async () => {
+    await sut(PokemonParams)
+
+    expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'post', body: PokemonParams })
+    expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+})
