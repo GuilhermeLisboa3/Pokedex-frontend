d8840e1e00f4e444052cd62d0d6545ab27cb6f9a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-11 11:07:14 -0300

    refactor: ensure CardPokemon add red heart if the pokemon is a favorite

--- src/application/components/card-pokemon/card-pokemon.tsx
+++ src/application/components/card-pokemon/card-pokemon.tsx
@@ -1,17 +1,21 @@
 import './styles.scss'
+import { PokemonContext } from '@/application/contexts'
 import { type Pokemon } from '@/domain/models'
+import { useContext } from 'react'
 
-import { FaRegHeart } from 'react-icons/fa'
+import { FaRegHeart, FaHeart } from 'react-icons/fa'
 
 type Props = { pokemon: Pokemon }
 
 export const CardPokemon: React.FC<Props> = ({ pokemon }: Props) => {
+  const { pokemonFavorite } = useContext(PokemonContext)
+  const isFavorite = pokemonFavorite(pokemon.name)
   const typePokemon = (position: number): string => pokemon.types[position].type.name
   return (
     <>
       <div className='card-pokemon'>
         <img className='card-pokemon-img-pokemon' src={pokemon.sprites.front_default} alt={pokemon.name}/>
-        <button className='card-pokemon-button-favorite'> <FaRegHeart className='favorite'/> </button>
+        <button className='card-pokemon-button-favorite'>{ isFavorite ? <FaHeart className='icon-favorite-red'/> : <FaRegHeart className='icon-favorite'/>}</button>
         <p className='card-pokemon-id-pokemon'>Nº{pokemon.id}</p>
         <p className='card-pokemon-name-pokemon'>{pokemon.name.substr(0, 16)}</p>
         <div className='card-pokemon-types-pokemon'>
--- tests/application/components/card-pokemon.spec.tsx
+++ tests/application/components/card-pokemon.spec.tsx
@@ -1,5 +1,6 @@
 import { PokemonParams } from '@/tests/mocks'
 import { CardPokemon } from '@/application/components'
+import { PokemonProvider } from '@/application/contexts'
 
 import React from 'react'
 import { render, screen } from '@testing-library/react'
@@ -7,10 +8,15 @@ import faker from 'faker'
 
 describe('CardPokemon', () => {
   it('should render with correct values', () => {
-    const { container } = render(<CardPokemon pokemon={{ ...PokemonParams, name: 'any_name' }}/>)
+    const { container } = render(
+      <PokemonProvider listFavoritePokemon={[PokemonParams]}>
+        <CardPokemon pokemon={{ ...PokemonParams, name: 'any_name' }}/>
+      </PokemonProvider>
+    )
     expect(screen.getByRole('img')).toHaveAttribute('src', PokemonParams.sprites.front_default)
     expect(screen.getByRole('img')).toHaveAttribute('alt', 'any_name')
     expect(container.querySelector('.card-pokemon-name-pokemon')).toHaveTextContent('any_name')
+    expect(container.querySelector('.icon-favorite')).toBeInTheDocument()
     expect(container.querySelector('.card-pokemon-id-pokemon')).toHaveTextContent(PokemonParams.id)
     expect(container.querySelector('.card-pokemon-types-pokemon')?.children).toHaveLength(1)
     expect(container.querySelector('.card-pokemon-types-pokemon')?.children[0]).toHaveTextContent(PokemonParams.types[0].type.name)
@@ -20,11 +26,16 @@ describe('CardPokemon', () => {
   it('should render with correct values and two typesPokemon', () => {
     const type1 = faker.name.findName()
     const type2 = faker.name.findName()
-    const { container } = render(<CardPokemon pokemon={{ ...PokemonParams, types: [{ type: { name: type1 } }, { type: { name: type2 } }], name: 'any_name' }}/>)
+    const { container } = render(
+      <PokemonProvider listFavoritePokemon={[{ ...PokemonParams, name: 'any_name' }]}>
+        <CardPokemon pokemon={{ ...PokemonParams, types: [{ type: { name: type1 } }, { type: { name: type2 } }], name: 'any_name' }}/>
+      </PokemonProvider>
+    )
     expect(screen.getByRole('img')).toHaveAttribute('src', PokemonParams.sprites.front_default)
     expect(screen.getByRole('img')).toHaveAttribute('alt', 'any_name')
     expect(container.querySelector('.card-pokemon-name-pokemon')).toHaveTextContent('any_name')
     expect(container.querySelector('.card-pokemon-id-pokemon')).toHaveTextContent(PokemonParams.id)
+    expect(container.querySelector('.icon-favorite-red')).toBeInTheDocument()
     expect(container.querySelector('.card-pokemon-types-pokemon')?.children).toHaveLength(2)
     expect(container.querySelector('.card-pokemon-types-pokemon')?.children[0]).toHaveTextContent(type1)
     expect(container.querySelector('.card-pokemon-types-pokemon')?.children[1]).toHaveTextContent(type2)
