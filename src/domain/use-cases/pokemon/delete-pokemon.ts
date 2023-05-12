16ae5bebaaeff94e169ca310fff62eb350d6f57f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-12 16:58:27 -0300

    feat: ensure DeletePokemonUseCase delete an pokemon if HttpClient return 204

--- src/domain/use-cases/pokemon/delete-pokemon.ts
+++ src/domain/use-cases/pokemon/delete-pokemon.ts
@@ -8,7 +8,7 @@ export type DeletePokemon = (input: Input) => Promise<void>
 export const DeletePokemonUseCase: Setup = (url, httpClient) => async ({ idPokemon }) => {
   const { statusCode } = await httpClient.request({ url: `${url}/${idPokemon}`, method: 'delete' })
   switch (statusCode) {
-    case 204: return undefined
+    case 204: break
     case 403: throw new AccessDeniedError()
     default: throw new UnexpectedError()
   }
