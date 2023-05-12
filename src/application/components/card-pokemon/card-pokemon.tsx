0926fd4d9fb57e1e495401645b2282fbeff4a15d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-12 11:37:13 -0300

    refactor: change cardPokemon style

--- src/application/components/card-pokemon/card-pokemon.tsx
+++ src/application/components/card-pokemon/card-pokemon.tsx
@@ -13,9 +13,10 @@ export const CardPokemon: React.FC<Props> = ({ pokemon }: Props) => {
   const typePokemon = (position: number): string => pokemon.types[position].type.name
   return (
     <>
+    <div className='container-card-pokemon'>
+      <button className='card-pokemon-button-favorite'>{ isFavorite ? <FaHeart className='icon-favorite-red'/> : <FaRegHeart className='icon-favorite'/>}</button>
       <div className='card-pokemon' onClick={() => { getDataPokemon(pokemon.name) }} data-testid='card-pokemon'>
         <img className='card-pokemon-img-pokemon' src={pokemon.sprites.front_default} alt={pokemon.name}/>
-        <button className='card-pokemon-button-favorite'>{ isFavorite ? <FaHeart className='icon-favorite-red'/> : <FaRegHeart className='icon-favorite'/>}</button>
         <p className='card-pokemon-id-pokemon'>Nº{pokemon.id}</p>
         <p className='card-pokemon-name-pokemon'>{pokemon.name.substr(0, 16)}</p>
         <div className='card-pokemon-types-pokemon'>
@@ -23,6 +24,7 @@ export const CardPokemon: React.FC<Props> = ({ pokemon }: Props) => {
           {pokemon.types.length > 1 ? <span className={`type ${typePokemon(1)}`}>{typePokemon(1)}</span> : ''}
         </div>
       </div>
+    </div>
     </>
   )
 }
