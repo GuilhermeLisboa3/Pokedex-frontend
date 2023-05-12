7608f7a58dd57177b4aed0f79c5f325e2a614b06
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-12 16:30:07 -0300

    test: ensure DeletePokemonUseCase call HttpClient with correct values

---
+++ tests/domain/use-cases/pokemon/delete-pokemon.spec.ts
@@ -0,0 +1,23 @@
+import { type DeletePokemon, DeletePokemonUseCase } from '@/domain/use-cases/pokemon'
+import { type HttpClient } from '@/domain/contracts/http'
+import { httpClientParams, PokemonParams } from '@/tests/mocks'
+
+import { mock } from 'jest-mock-extended'
+
+describe('DeletePokemonUseCase', () => {
+  let sut: DeletePokemon
+  const { idPokemon } = PokemonParams
+  const { url } = httpClientParams
+  const httpClient = mock<HttpClient>()
+
+  beforeEach(() => {
+    sut = DeletePokemonUseCase(url, httpClient)
+  })
+
+  it('should call HttpClient with correct values', async () => {
+    await sut({ idPokemon })
+
+    expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/${idPokemon}`, method: 'delete' })
+    expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+})
