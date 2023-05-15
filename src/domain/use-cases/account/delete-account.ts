20e3950cdffc67559e31f98de7b34caa29b244d6
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-15 14:18:33 -0300

    feat: ensure DeleteAccountUseCase throw AccessDeniedError HttpClient return 403

--- src/domain/use-cases/account/delete-account.ts
+++ src/domain/use-cases/account/delete-account.ts
@@ -1,8 +1,13 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { AccessDeniedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => DeleteAccount
 export type DeleteAccount = () => Promise<void>
 
 export const DeleteAccountUseCase: Setup = (url, httpClient) => async () => {
-  await httpClient.request({ url, method: 'delete' })
+  const { statusCode } = await httpClient.request({ url, method: 'delete' })
+  switch (statusCode) {
+    case 403: throw new AccessDeniedError()
+    default: return undefined
+  }
 }
