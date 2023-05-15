aea876d7d279179e67b492ab27dd669bb95fba63
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-15 14:19:22 -0300

    test: ensure DeleteAccountUseCase throw AccessDeniedError HttpClient return 403

--- tests/domain/use-cases/account/delete-account.spec.ts
+++ tests/domain/use-cases/account/delete-account.spec.ts
@@ -3,11 +3,16 @@ import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams } from '@/tests/mocks'
 
 import { mock } from 'jest-mock-extended'
+import { AccessDeniedError } from '@/domain/errors'
 
 describe('DeleteAccountUseCase', () => {
   let sut: DeleteAccount
   const { url } = httpClientParams
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 204 })
+  })
 
   beforeEach(() => {
     sut = DeleteAccountUseCase(url, httpClient)
@@ -18,5 +23,13 @@ describe('DeleteAccountUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'delete' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw AccessDeniedError HttpClient return 403', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 403 })
+
+    const promise = sut()
+
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
