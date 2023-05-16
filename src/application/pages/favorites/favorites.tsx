ff3b5be01c1a988b9767d5db86fde6e249735153
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-16 12:16:55 -0300

    feat: ensure Favorites call GetDataPokemon with correct value

--- src/application/pages/favorites/favorites.tsx
+++ src/application/pages/favorites/favorites.tsx
@@ -1,19 +1,33 @@
 import './styles.scss'
 import { Footer } from '@/application/components'
 import { type GetListFavoritePokemon } from '@/domain/use-cases/pokemon'
+import { type GetDataPokemon } from '@/domain/use-cases/api-pokemon'
+import { type Pokemon } from '@/domain/models'
 
 import { Container } from 'reactstrap'
 import Link from 'next/link'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 type Props = {
   getListFavoritePokemon: GetListFavoritePokemon
+  getDataPokemon: GetDataPokemon
 }
 
-export const Favorites: React.FC<Props> = ({ getListFavoritePokemon }: Props) => {
+export const Favorites: React.FC<Props> = ({ getListFavoritePokemon, getDataPokemon }: Props) => {
+  const [listFavoritePokemon, setListFavoritePokemon] = useState<Pokemon[]>([])
+
   useEffect(() => {
-    getListFavoritePokemon()
+    getListFavoritePokemon().then((result: Pokemon[]) => {
+      setListFavoritePokemon(result)
+      listPokemonHandler()
+    })
   })
+
+  const listPokemonHandler = async (): Promise<void> => {
+    listFavoritePokemon.map(async (pokemon) => {
+      await getDataPokemon({ name: pokemon.idPokemon })
+    })
+  }
 
   return (
     <>
