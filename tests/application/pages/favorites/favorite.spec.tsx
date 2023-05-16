3410022554639154e6cae07a0be5566da18c715c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-16 12:17:35 -0300

    test: ensure Favorites call GetDataPokemon with correct value

--- tests/application/pages/favorites/favorite.spec.tsx
+++ tests/application/pages/favorites/favorite.spec.tsx
@@ -1,23 +1,36 @@
 import { Favorites } from '@/application/pages/favorites/favorites'
+import { PokemonParams } from '@/tests/mocks'
 
 import React from 'react'
 import { render, waitFor, screen } from '@testing-library/react'
 
 describe('Favorites', () => {
   const getListFavoritePokemon = jest.fn()
+  const getDataPokemon = jest.fn()
   type SutTypes = { container: HTMLElement }
 
   const makeSut = (): SutTypes => {
     const { container } = render(
-      <Favorites getListFavoritePokemon={getListFavoritePokemon}/>
+      <Favorites getListFavoritePokemon={getListFavoritePokemon} getDataPokemon={getDataPokemon}/>
     )
     return { container }
   }
+
+  beforeAll(() => {
+    getListFavoritePokemon.mockResolvedValue([PokemonParams, PokemonParams])
+  })
 
   it('should call GetListFavoritePokemon', async () => {
     makeSut()
     expect(getListFavoritePokemon).toHaveBeenCalled()
     expect(getListFavoritePokemon).toHaveBeenCalledTimes(1)
     await waitFor(() => screen.getByRole('img'))
+  })
+
+  it('should call GetDataPokemon with correct value', async () => {
+    makeSut()
+    await waitFor(() => screen.getByRole('img'))
+    expect(getDataPokemon).toHaveBeenCalled()
+    expect(getDataPokemon).toHaveBeenCalledTimes(2)
   })
 })
