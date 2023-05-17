dee65f7f0ed56557e359df1362e19069d5e3de4d
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-05-17 13:58:22 -0300

    feat: ensure Favorites present list of favorite pokemons

--- tests/e2e/cypress/integration/favorites.cy.ts
+++ tests/e2e/cypress/integration/favorites.cy.ts
@@ -1,7 +1,9 @@
-import { mockUnexpectedError } from '../mocks/http-mocks'
+import { mockOk, mockUnexpectedError } from '../mocks/http-mocks'
 
 describe('Favorites', () => {
-  const mockError = (method: any): void => { method('GET', /pokemon?/) }
+  const mockError = (method: any): void => { method('GET', /pokemons?/) }
+  const mockFavoritePokemon = (): void => { mockOk('GET', /pokemons/, 'favorite-pokemon') }
+  const mockDataPokemon = (): void => { mockOk('GET', /pokemon[/]/, 'pokemon') }
 
   beforeEach(() => {
     cy.fixture('login').then(account => cy.setLocalStorageItem('pokemon-token', account))
@@ -12,5 +14,16 @@ describe('Favorites', () => {
     cy.visit('favorites')
 
     cy.get('.error span').should('have.text', 'Algo deu errado. Tente novamente!')
+  })
+
+  it('should present list of favorite pokemons', () => {
+    mockFavoritePokemon()
+    mockDataPokemon()
+    cy.visit('favorites')
+
+    cy.get('.card-pokemon-img-pokemon').should('have.attr', 'src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png')
+    cy.get('.card-pokemon-id-pokemon').should('have.text', 'Nº9')
+    cy.get('.card-pokemon-name-pokemon').should('have.text', 'blastoise')
+    cy.get('.type').should('have.text', 'water')
   })
 })
